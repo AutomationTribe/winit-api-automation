2ee3ecb231a48c6a80dc3738974e5e3499f1162e
Mataga <mataga@renmoney.com>
2025-03-21 18:55:21 +0100

    add send otp to phone number

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges:false,
   e2e: {
+    baseUrl:"https://winit.sbscuk.co.uk/public",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/send otp phone number.cy.js
@@ -0,0 +1,83 @@
+describe("Send otp to phone test suite",()=>{
+
+    let options,data;
+    let url = Cypress.config('baseUrl');
+
+
+    before("Load user data",function(){
+        cy.fixture('user').then((res)=>{
+            data = res;
+        })
+    })
+
+      it("should  send an OTP for a valid phone number",()=>{
+
+        options = {
+            method: "post",
+            url: url + `/api/v1/auth/send-otp-phone`,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body : {
+                "phone_number" : data.phoneNumber
+            }
+        }
+
+        cy.request(options).then((res)=>{
+
+            expect(res.status).to.eq(200);
+            expect(res.body.message).to.eq("OTP Sent To Phone Successfully.");
+            expect(res.body).to.have.property("data").and.not.null 
+
+        });
+    })
+
+
+    it("should not send an OTP for an empty phone number",()=>{
+
+        options = {
+            method: "post",
+            failOnStatusCode: false,
+            url: url + `/api/v1/auth/send-otp-phone`,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body : {
+                "phone_number" : null,
+            },
+
+        }
+
+        cy.request(options).then((res)=>{
+            expect(res.status).to.eq(422);
+           
+        });
+    })
+
+    
+    it("should not send an OTP for a invalid phone number",()=>{
+
+        options = {
+            method: "post",
+            failOnStatusCode:false,
+            url: url + `/api/v1/auth/send-otp-phone`,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body : {
+                "phone_number" : "0706818192828282"
+            },
+    
+        }
+
+        cy.request(options).then((res)=>{
+
+            expect(res.status).to.eq(422);
+           
+
+        });
+    })
+})
